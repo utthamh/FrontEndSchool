84e6c917cb40b4b52a3d10cf392bf41f0219ece7
revan <utthamcsc@gmail.com>
2020-07-24 13:50:53 +0530

    Updates

---
+++ precache-manifest.6eed12fb926c2bc08a5032b38ccd223f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c393392f439f7139ff1c6e2f1a6d94f",
+    "url": "/FrontEndSchool/index.html"
+  },
+  {
+    "revision": "8f549d8648d3726d6dad",
+    "url": "/FrontEndSchool/static/css/main.cd7075c4.chunk.css"
+  },
+  {
+    "revision": "f537a8e98e4dc2a118b7",
+    "url": "/FrontEndSchool/static/js/2.12e67188.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FrontEndSchool/static/js/2.12e67188.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f549d8648d3726d6dad",
+    "url": "/FrontEndSchool/static/js/main.7222d774.chunk.js"
+  },
+  {
+    "revision": "7e04677f23c4c6bbea12",
+    "url": "/FrontEndSchool/static/js/runtime-main.43fcd551.js"
+  },
+  {
+    "revision": "9ca2e6ae7dbc68dc945b67ea13132b34",
+    "url": "/FrontEndSchool/static/media/about.9ca2e6ae.jpg"
+  },
+  {
+    "revision": "116fddc0ffc2c649c7e8018e77537aef",
+    "url": "/FrontEndSchool/static/media/hero-bg.116fddc0.jpg"
+  },
+  {
+    "revision": "6199a2ce6421905f001e3046c02bf336",
+    "url": "/FrontEndSchool/static/media/trainer-1.6199a2ce.jpg"
+  },
+  {
+    "revision": "2451fb86051ef9b388b03dbd97953927",
+    "url": "/FrontEndSchool/static/media/trainer-2.2451fb86.jpg"
+  },
+  {
+    "revision": "2d3913707e8be39dca0d27db9d05aebb",
+    "url": "/FrontEndSchool/static/media/trainer-3.2d391370.jpg"
+  }
+]);
